510af37015faee565e02eda9451074f003e5a0b0
KyleJ <kjacobson351@gmail.com>
2023-07-18 16:59:42 +0900

    add generative icons to card, add color picker to form, add 1/4 time field to form

--- scripts/script.js
+++ scripts/script.js
@@ -23,18 +23,22 @@ function addCarToGarage(){
   let make = document.querySelector("#make").value;
   let model = document.querySelector("#model").value;
   let type = document.querySelector("#type").value;
+  let color = document.querySelector("#color").value;
   let engineSize = document.querySelector("#engine").value;
-  let newVehicle = new Vehicle (year, make, model, type, engineSize);
+  let quarterMile = document.querySelector("#quarter-mile").value;
+  let newVehicle = new Vehicle (year, make, model, type, color, engineSize, quarterMile);
   vehicleArray.push(newVehicle);
   
 }
 
-function Vehicle (year, make, model, type, engineSize) {
+function Vehicle (year, make, model, type, color, engineSize, quarterMile) {
   this.year = year;
   this.make = make;
   this.model = model;
   this.type = type;
+  this.color = color;
   this.engineSize = engineSize;
+  this.quarterMile = quarterMile;
 }
 
 //erases all cards from screen then makes a new card for every vehicle in the vehicles array
@@ -45,12 +49,17 @@ function render(){
     cardWrapper.setAttribute("class","card-wrapper");
     const yearMake = document.createElement("div");
     yearMake.setAttribute("class","year-make");
+    const iconDiv = document.createElement("div");
+    iconDiv.setAttribute("class","icon");
     const type = document.createElement("div");
     type.setAttribute("class","card-other");
     type.setAttribute("id","type");
     const engineSize = document.createElement("div");
     engineSize.setAttribute("class","card-other");
     engineSize.setAttribute("id","engine-size");
+    const quarterMile = document.createElement("div");
+    quarterMile.setAttribute("class","card-other");
+    quarterMile.setAttribute("id","quarter-mile");
     const cardButtonDiv = document.createElement("div");
     cardButtonDiv.setAttribute("class","card-buttons");
     const selectBtn = document.createElement("button");
@@ -63,18 +72,44 @@ function render(){
     deleteBtn.setAttribute("onClick", "deleteCard(this)")
     deleteBtn.innerText="Delete";
     //appending
+    
     cardWrapper.appendChild(yearMake);
+    
     cardWrapper.appendChild(type);
     cardWrapper.appendChild(engineSize);
+    cardWrapper.appendChild(quarterMile);
     cardWrapper.appendChild(cardButtonDiv);
     cardButtonDiv.appendChild(selectBtn);
     cardButtonDiv.appendChild(deleteBtn);
     main.appendChild(cardWrapper)
+    
     //sets the html from each card using the current info from the current object of the iteration
     const current = vehicleArray[i];
     yearMake.innerText =`${current.year} ${current.make} ${current.model}`;
     type.innerText = current.type;
+    yearMake.appendChild(iconDiv);//MOVED THIS LINE DOWN
+    switch (current.type) {
+      case "Car":
+        iconDiv.innerHTML = `<i class="fa-solid fa-car-side"></i>`;
+        break;
+        case "Truck":
+        iconDiv.innerHTML = `<i class="fa-solid fa-truck-pickup"></i>`;
+        break;
+      case "Van":
+        iconDiv.innerHTML = `<i class="fa-solid fa-van-shuttle"></i>`;
+        break;
+        case "Motorcycle":
+        iconDiv.innerHTML = `<i class="fa-solid fa-motorcycle"></i>`;
+        break;
+    }
+    
+    /*if (current.type === "Van"){//turn this into switch statment
+      iconDiv.innerHTML = `<i class="fa-solid fa-van-shuttle"></i>`;
+    }*/
+    iconDiv.style.color = current.color;
     engineSize.innerText = current.engineSize;
+    quarterMile.innerText = `1/4 mile: ${current.quarterMile} seconds`;
+    //iconDiv.innerText="test";
    
   }
 }
